e78ded396de927d104806a80e221fec96333e630
Joey155396 <15168200181@163.com>
2016-10-19 17:44:29 +0800

    1.0.3
    嗷嗷嗷啊啊

--- dagndang-app-angularjs/assets/controllers/main_controller.js
+++ dagndang-app-angularjs/assets/controllers/main_controller.js
@@ -2,5 +2,7 @@ app.controller('mainController',['$scope','commonService',function ($scope,c_s) 
   c_s.getData('fairytale',function (res) {
     console.dir(res);
   });
-
+  $scope.linkTo = function (index) {
+    $scope.selectedIndex = index;
+  };
 }]);
