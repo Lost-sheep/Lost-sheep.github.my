52a67a4b8e0074bd4c54e80bac993ae7fe2563f5
Joey155396 <15168200181@163.com>
2016-10-21 09:14:27 +0800

    1.0.5
    啊啊啊啊

--- dagndang-app-angularjs/assets/controllers/allBook_controller.js
+++ dagndang-app-angularjs/assets/controllers/allBook_controller.js
@@ -1,4 +1,5 @@
 app.controller('booklistController',['$scope','commonService',function ($scope,commonService) {
+   console.log($routeParams.id);
   commonService.getData($routeParams.id ,function(res) {
       $scope.bookData=res.data;
     });
